1912cfad6079ab7a396cdbbd072b5db91294f385
Roesn <Bubwub04@icloud.com>
2023-10-28 17:02:21 -0400

    fixed port err

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 // set port
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 // import our modular routers for api 
 const api = require('./routes/index.js');
