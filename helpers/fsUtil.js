da7ce91bb90cd34f495e7e3b35d7cdc3e07363a1
Roesn <Bubwub04@icloud.com>
2023-10-28 10:10:15 -0400

    fs read and append and write func

---
+++ helpers/fsUtil.js
@@ -0,0 +1,24 @@
+// require fs
+const fs = require('fs');
+
+// write to file (replaces content)
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+// read and append reads file and appends to end 
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+// export module
+module.exports = { writeToFile, readAndAppend };
